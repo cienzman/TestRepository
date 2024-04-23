1cbcbd76eadacf646bb6e321fe9548b4e12e82a2
cienzman <151162645+cienzman@users.noreply.github.com>
2024-04-23 14:50:46 +0200

    Update ClassController1.java

--- TestGitHub/src/Controller/ClassController1.java
+++ TestGitHub/src/Controller/ClassController1.java
@@ -2,7 +2,7 @@ package Controller;
 
 public class ClassController1 {
 	public ClassController1() {
-		System.out.println("CLASS1Controller1");
+		System.out.println("CLASS1Controller1 I WANT TO EDIT THIS");
 	}
 
 	
